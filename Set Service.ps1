f7447c5b1cbab9ee6239dc72ce15fd70ae20f5ea
epanting <eliopanting@gmail.com>
2017-02-21 15:56:28 -0500

    Add Test PowerShell script

---
+++ Set Service.ps1
@@ -0,0 +1,8 @@
+﻿#Import-Module ServerManager
+#$Servers = Import-Csv C:\Temp\Servers1.csv -UseCulture
+$Servers = 'CTS1','CTS2','CTS3','CTS4', 'CTS5','CTS6','CTS7','CTS8','TRTS1','DRAGONTS', 'TRAININGTS'
+ForEach ($Server in $Servers) {
+  Set-Service -Name Themes -ComputerName $Server -StartupType Automatic -Status Running }
+
+
+	
