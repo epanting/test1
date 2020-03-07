3096577e1f7104783b743dba5747944d3fa9f47e
Elio F. Panting <epanting@workforcelogiq.com>
2020-03-06 21:02:00 -0500

    test commit

--- Set Service.ps1
+++ Set Service.ps1
@@ -1,4 +1,4 @@
-﻿#Import-Module ServerManager
+﻿Import-Module ServerManager
 #$Servers = Import-Csv C:\Temp\Servers1.csv -UseCulture
 $Servers = 'Server1','Server2','Server3','Server4', 'Server5','Server6','Server7','Server8'
 ForEach ($Server in $Servers) {
