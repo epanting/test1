a506541598d4618fbe0865e4127b972390735863
Elio F. Panting <epanting@workforcelogiq.com>
2020-03-06 20:43:01 -0500

    My test commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## This is a test repository
 
 This was created in order to learn to use github integrated with Windows and Linux
+
+This modification was done from MS Visual Studio Code
--- Set Service.ps1
+++ Set Service.ps1
@@ -1,5 +1,5 @@
 ﻿#Import-Module ServerManager
 #$Servers = Import-Csv C:\Temp\Servers1.csv -UseCulture
-$Servers = 'Server1','Server2','Server3','Server4', 'Server5','Server6','Server7','Server8','TRTS1','DRAGONTS', 'TRAININGTS'
+$Servers = 'Server1','Server2','Server3','Server4', 'Server5','Server6','Server7','Server8'
 ForEach ($Server in $Servers) {
   Set-Service -Name Themes -ComputerName $Server -StartupType Automatic -Status Running }
